2a2a76e31ecfec78e50c6b1da4efd968d04cb513
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-08-26 08:46:33 +0000

    SVN initial import
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@16 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Demos/Hello/Hello.dpr
@@ -0,0 +1,50 @@
+program Hello;
+
+uses
+   Dialogs,
+   dwsComp,             // this is where TDelphiWebScript is found
+   dwsExprs,            // this is where TdwsProgram is found
+   dwsVCLGUIFunctions;  // registers the ShowMessaqe internal function
+
+procedure DwsHello;
+var
+   dws : TDelphiWebScript;
+   prog : TdwsProgram;
+begin
+   // create the compiler component
+   // internal functions (like ShowMessage) register at a global level
+   // specific functions on the other hand are handled via TdwsUnit, and have
+   // to be linked to each compiler (we don't have any in this sample)
+   dws:=TDelphiWebScript.Create(nil);
+   try
+      // compiles the script into a program
+      prog:=dws.Compile( 'var s : String = ''Hello World!'';'#13#10
+                        +'ShowMessage(s);');
+      try
+         // if there were errors, hints or warnings, you'll find them in the Msgs
+         if prog.Msgs.Count=0 then begin
+
+            // no compilation problem, we can run the script
+            prog.Execute;
+            // in a more complex case, you may want to protect execution with
+            // a try-except, to catch script exception (done in the script with a raise)
+            // or you own exception (happening in Delphi code invoked from the script)
+
+         end else begin
+
+            // display the compilation problems
+            // you'll have to introduce errors in the above script to get there ;)
+            ShowMessage(prog.Msgs.AsInfo);
+
+         end;
+      finally
+         prog.Free;
+      end;
+   finally
+      dws.Free;
+   end;
+end;
+
+begin
+   DwsHello;
+end.
