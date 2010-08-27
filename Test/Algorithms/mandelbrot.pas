51bf579c5cdb95fad5039468c88a212dbac8c379
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-08-27 06:14:10 +0000

    SVN initial import
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@27 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/Algorithms/mandelbrot.pas
@@ -0,0 +1,23 @@
+var i, j, newColor : Integer;
+var rU, rV, rX, rY, rZ : Float;
+
+const cSize = 80;
+const cAscii = '0123456789.';
+
+for i := 0 to cSize-2 do begin
+   for j := 0 to cSize-2 do begin
+      rX := -0.8 + 3 * i / cSize;
+      rY := -1.4 + 2.8 * j / cSize;
+      newColor := 1;
+      rU := 0;
+      rV := 0;
+      repeat
+         rZ := Sqr(rU) - Sqr(rV) - rX;
+         rV := 2 * rU * rV - rY;
+         rU := rZ;
+         newColor := newColor + 1;
+      until (Sqr(rU) + Sqr(rV) > 9) or (newColor = 11);
+      Print(cAscii[newColor]);
+   end;
+   PrintLn('');
+end;
