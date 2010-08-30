0152082cd09feb6f2fa53122d7180a0b249a2b95
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-08-30 08:20:44 +0000

    SVN initial import
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@41 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/SimpleScripts/string_manip.pas
@@ -0,0 +1,13 @@
+const empty = '';
+var str : String = empty;
+
+PrintLn(Length(str));
+SetLength(str, 1);
+PrintLn(Length(str));
+str[1]:='z';
+PrintLn(str);
+str:=str+'a';
+str:='o'+str;
+PrintLn(str);
+
+PrintLn('df'+'01'+str+'13'+'45'+str+empty+str);
