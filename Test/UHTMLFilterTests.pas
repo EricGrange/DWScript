dce4c60b382f0b0c3b65ab5230538eb758dd7299
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-09-24 16:03:12 +0000

    Clarified failed checks
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@105 d49b5f25-dba9-2846-803f-502412c64022

--- Test/UHTMLFilterTests.pas
+++ Test/UHTMLFilterTests.pas
@@ -9,26 +9,29 @@ uses
   dwsComp,
   dwsCompiler,
   dwsExprs,
-  dwshtmlfilter;
+  dwsHtmlfilter;
 
 type
 
-  THTMLFilterTests = class(TTestCase)
-  private
-    FTests: TStringList;
-    FCompiler: TDelphiWebScript;
-    FFilter: TdwsHTMLFilter;
-    FUnit: TdwsHTMLUnit;
-  public
-    procedure CollectFiles(const directory, fileMask: string; list: TStrings);
+   THTMLFilterTests = class(TTestCase)
+   private
+      FTests: TStringList;
+      FCompiler: TDelphiWebScript;
+      FFilter: TdwsHTMLFilter;
+      FUnit: TdwsHTMLUnit;
 
-    procedure SetUp; override;
-    procedure TearDown; override;
+   public
+      procedure CollectFiles(const directory, fileMask: string; list: TStrings);
 
-    procedure DoInclude(const scriptName: string; var scriptSource: string);
-  published
-    procedure TestHTMLScript;
-  end;
+      procedure SetUp; override;
+      procedure TearDown; override;
+
+      procedure DoInclude(const scriptName: string; var scriptSource: string);
+
+   published
+      procedure TestHTMLScript;
+
+   end;
 
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
@@ -71,11 +74,14 @@ begin
 
   FCompiler := TDelphiWebScript.Create(nil);
   FCompiler.OnInclude := DoInclude;
+
   FFilter := TdwsHTMLFilter.Create(nil);
   FFilter.PatternOpen := '<?pas';
   FFilter.PatternClose := '?>';
   FFilter.PatternEval := '=';
+
   FCompiler.Config.Filter := FFilter;
+
   FUnit := TdwsHTMLUnit.Create(nil);
   FCompiler.AddUnit(FUnit);
 end;
@@ -93,19 +99,23 @@ end;
 procedure THTMLFilterTests.TestHTMLScript;
 var
   s: string;
+  resultFileName : String;
   prog: TdwsProgram;
   sl : TStringList;
 begin
    sl:=TStringList.Create;
    try
-      for s in FTests do
-      begin
+      for s in FTests do begin
          sl.LoadFromFile(s);
-        prog := FCompiler.Compile(sl.Text);
-        CheckEquals(0, prog.Msgs.Count);
-        prog.Execute;
-        sl.LoadFromFile(ChangeFileExt(s, '.txt'));
-        CheckEquals(sl.Text, (prog.Result as TdwsDefaultResult).Text);
+         prog := FCompiler.Compile(sl.Text);
+         CheckEquals('', prog.Msgs.AsInfo, s);
+         prog.Execute;
+
+         resultFileName:=ChangeFileExt(s, '.txt');
+         if FileExists(resultFileName) then
+            sl.LoadFromFile(ChangeFileExt(resultFileName, '.txt'))
+         else sl.Clear;
+         CheckEquals(sl.Text, (prog.Result as TdwsDefaultResult).Text, s);
       end;
    finally
       sl.Free;
