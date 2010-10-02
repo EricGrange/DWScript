ae5f13397d7bc96716ca715219470f0c9f16ed38
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-02 12:21:37 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@133 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/FailureScripts/bitwise_booleans.pas
@@ -0,0 +1,3 @@
+PrintLn(FloatToStr(3.14 And 5));
+PrintLn(FloatToStr(5 or 3.14));
+PrintLn(FloatToStr(5 xor '3'));
---
+++ Test/FailureScripts/bitwise_shift.pas
@@ -0,0 +1,2 @@
+PrintLn(FloatToStr(3.14 Shr 2));
+PrintLn(FloatToStr(2 shl 3.14));
