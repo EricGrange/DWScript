559e2e03ac037822c16bdcadaa0f14c3cd4376a4
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-05 13:40:01 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@147 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/SimpleScripts/variant_ops.pas
@@ -0,0 +1,14 @@
+var vi : Variant = 5;
+var vf : Variant = 5.5;
+
+PrintLn(vf*3.5);
+PrintLn(Round(vf*3));
+
+PrintLn(3.5*vf);
+PrintLn(Round(3*vf));
+
+PrintLn(vi*3.5);
+PrintLn(Round(vi*3));
+
+PrintLn(3.5*vi);
+PrintLn(Round(3*vi));
