d98edbbca2dce52b2220f99ba66b2d8c6e4789a8
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-06 08:25:56 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@156 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/SimpleScripts/format.pas
@@ -0,0 +1,13 @@
+PrintLn(Format('hello', []));
+PrintLn(Format('hello %s', ['world']));
+PrintLn(Format('hello %s %d', ['world', 123]));
+PrintLn(Format('hello %s %d %f', ['world', 123, 12.3]));
+
+var s = 'WORLD';
+var i = 456;
+var f = 3.14;
+
+PrintLn(Format('hello %s', [s]));
+PrintLn(Format('hello %s %d', [s, i]));
+PrintLn(Format('hello %s %d %f', [s, i, f]));
+
