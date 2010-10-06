4a1d7e0bf76d198665cefb8b058abc94370343d4
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-06 08:26:09 +0000

    Extended tests
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@157 d49b5f25-dba9-2846-803f-502412c64022

--- Test/SimpleScripts/variant_ops.pas
+++ Test/SimpleScripts/variant_ops.pas
@@ -1,5 +1,6 @@
 var vi : Variant = 5;
 var vf : Variant = 5.5;
+var vs : Variant = 'world';
 
 PrintLn(vf*3.5);
 PrintLn(Round(vf*3));
@@ -12,3 +13,16 @@ PrintLn(Round(vi*3));
 
 PrintLn(3.5*vi);
 PrintLn(Round(3*vi));
+
+PrintLn('hello '+vs);
+PrintLn(vs+' hello');
+
+PrintLn(55/vf);
+PrintLn(vf/5);
+
+PrintLn(10-vf);
+PrintLn(vf-5);
+
+PrintLn(vi-vf);
+PrintLn(vf-vi);
+
