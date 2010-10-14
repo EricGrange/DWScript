af4f6e5b537e02daebdf2d809a1f8bc6671012f2
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-14 13:16:03 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@177 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/SimpleScripts/variants_casts.pas
@@ -0,0 +1,39 @@
+var v : Variant;
+var s : String;
+var f : Float;
+var i : Integer;
+var b : Boolean;
+
+s:='abcd';
+v:=s;
+PrintLn(v);
+v:='efgh';
+s:=v;
+PrintLn(s);
+PrintLn(v);
+
+f:=1.234;
+v:=f;
+PrintLn(v);
+v:=5.678;
+f:=v;
+PrintLn(FloatToStr(f));
+PrintLn(FloatToStr(v));
+
+i:=1234;
+v:=i;
+PrintLn(v);
+v:=5678;
+i:=v;
+PrintLn(IntToStr(i));
+PrintLn(IntToStr(v));
+
+b:=True;
+v:=b;
+PrintLn(v);
+v:=False;
+b:=v;
+PrintLn(b);
+PrintLn(v);
+
+
