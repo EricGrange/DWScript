386c8645ba371e47c483adb4da8340890c8808af
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-14 11:57:19 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@173 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/ClassesLib/TStringBuilder.pas
@@ -0,0 +1,14 @@
+var sb = TStringBuilder.Create;
+
+PrintLn('<'+sb.ToString+'>');
+sb.Append('hello');
+PrintLn('<'+sb.ToString+'>');
+sb.Append(' world');
+PrintLn('<'+sb.ToString+'>');
+
+sb.Clear;
+sb.Append(123);
+PrintLn('<'+sb.ToString+'>');
+sb.Append(',').Append(3.14);
+PrintLn('<'+sb.ToString+'>');
+
