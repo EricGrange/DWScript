b5b0267bb61cf3178a9c0a61bc45319d0439d3c7
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-10-21 07:06:55 +0000

    SVN Initial
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@213 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Demos/dwsRunner/dwsRunner.dpr
@@ -0,0 +1,55 @@
+program dwsRunner;
+
+{$WEAKLINKRTTI ON}
+{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+{$APPTYPE CONSOLE}
+
+uses
+   Classes, SysUtils, dwsComp, dwsCompiler, dwsExprs,
+   dwsMathFunctions, dwsStringFunctions, dwsTimeFunctions, dwsVariantFunctions;
+
+var
+   sl : TStringList;
+   script : TDelphiWebScript;
+   prog : TdwsProgram;
+   i : Integer;
+   params : array of Variant;
+begin
+   if ParamCount<1 then begin
+      Writeln('dwsRunner <sourcefile> [param1] [param2] ... [paramN]');
+      Exit;
+   end;
+   if not FileExists(ParamStr(1)) then begin
+      Writeln('File "', ParamStr(1), '" not found.');
+      Exit;
+   end;
+   try
+      script:=TDelphiWebScript.Create(nil);
+      sl:=TStringList.Create;
+      try
+         sl.LoadFromFile(ParamStr(1));
+         prog:=script.Compile(sl.Text);
+         try
+            if prog.Msgs.Count>0 then begin
+               Writeln(prog.Msgs.AsInfo);
+            end else begin
+               SetLength(params, ParamCount-1);
+               for i:=2 to ParamCount do
+                  params[i-2]:=ParamStr(i);
+               prog.ExecuteParam(params);
+               Writeln((prog.Result as TdwsDefaultResult).Text);
+               if prog.Msgs.Count>0 then
+                  Writeln(prog.Msgs.AsInfo);
+            end;
+         finally
+            prog.Free;
+         end;
+      finally
+         sl.Free;
+         script.Free;
+      end;
+   except
+      on E: Exception do
+         Writeln(E.ClassName, ': ', E.Message);
+   end;
+end.
