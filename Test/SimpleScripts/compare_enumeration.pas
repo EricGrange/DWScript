b008fa80a903e9df2f301650ca59bfc995fc9989
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2010-12-21 15:10:34 +0000

    Added in/not in test
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@373 d49b5f25-dba9-2846-803f-502412c64022

--- Test/SimpleScripts/compare_enumeration.pas
+++ Test/SimpleScripts/compare_enumeration.pas
@@ -14,3 +14,9 @@ PrintBool('1 <> 1 is ', enOne<>enOne);
 
 PrintBool('1 = 2 is ', enOne=enTwo);
 PrintBool('1 <> 2 is ', enOne<>enTwo);
+
+PrintBool('1 in [2, 3] is ', enOne in [enTwo, enThree]);
+PrintBool('1 not in [2, 3] is ', enOne not in [enTwo, enThree]);
+
+PrintBool('2 in [2, 3] is ', enTwo in [enTwo, enThree]);
+PrintBool('2 not in [2, 3] is ', enTwo not in [enTwo, enThree]);
