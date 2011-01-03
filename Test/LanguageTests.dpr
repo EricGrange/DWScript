be0a387b2931948c58062d13e8020f982f06e124
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2011-01-03 12:51:54 +0000

    Report leaks by default
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@411 d49b5f25-dba9-2846-803f-502412c64022

--- Test/LanguageTests.dpr
+++ Test/LanguageTests.dpr
@@ -6,6 +6,7 @@ program LanguageTests;
 {$ENDIF}
 
 uses
+   FastMM4,
   Classes,
   Forms,
   TestFrameWork,
@@ -28,6 +29,7 @@ uses
 {$R *.res}
 
 begin
+   ReportMemoryLeaksOnShutdown:=True;
    Application.Initialize;
    GUITestRunner.RunRegisteredTests;
 end.
