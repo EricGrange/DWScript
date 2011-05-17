40bff2204ede31b65e724f25d5260322ce142237
zarglu <zarglu@d49b5f25-dba9-2846-803f-502412c64022>
2011-05-17 16:00:07 +0000

    Tweaked test
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@638 d49b5f25-dba9-2846-803f-502412c64022

--- Test/SimpleScripts/booleans.pas
+++ Test/SimpleScripts/booleans.pas
@@ -10,9 +10,9 @@ var f : Boolean = False or False;
 
 PrintLn(t and f);
 PrintLn(t or f);
-PrintOut(t xor f);
-PrintOut(t xor t);
-PrintOut(not t);
+PrintLn(t xor f);
+PrintLn(t xor t);
+PrintLn(not t);
 
 PrintLn(t and (f or t or f) and (t xor f) and not f);
 
