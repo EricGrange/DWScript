f3921793bfd400d430f94dd7d5587069e5790a80
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2011-07-26 07:01:15 +0000

    Differentiate float precision test cases between standard & extended (neg precision)
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@752 d49b5f25-dba9-2846-803f-502412c64022

--- Test/FunctionsString/float_precision_neg.pas
+++ Test/FunctionsString/float_precision_neg.pas
@@ -4,9 +4,9 @@ var f2 := 567.890123;
 var i : Integer;
 
 PrintLn(FloatToStr(f1));
-for i:=0 to 5 do
+for i:=-2 to 5 do
    PrintLn(FloatToStr(f1, i));
 
 PrintLn(FloatToStr(f2));
-for i:=0 to 5 do
+for i:=-2 to 5 do
    PrintLn(FloatToStr(f2, i));
