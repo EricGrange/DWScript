e7a887598efbc1ab7cf651c4a1bf3ae28788b050
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2011-08-23 07:30:28 +0000

    Made test less sensitive
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@824 d49b5f25-dba9-2846-803f-502412c64022

--- Test/Algorithms/max_recursion.pas
+++ Test/Algorithms/max_recursion.pas
@@ -11,4 +11,6 @@ end;
 
 Recursive;
 
-Println('Recursion Level is ' + IntToStr(level));
+if level>1000 then
+   Println('Recursion Level is above 1000')
+else Println('Recursion Level is under 1000');
