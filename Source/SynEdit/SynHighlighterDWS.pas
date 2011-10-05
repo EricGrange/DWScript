196ef0901528cb67ea71860caace371408fc78a0
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2011-10-05 13:28:34 +0000

    Removed SVN $Id tag
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@939 d49b5f25-dba9-2846-803f-502412c64022

--- Source/SynEdit/SynHighlighterDWS.pas
+++ Source/SynEdit/SynHighlighterDWS.pas
@@ -28,10 +28,6 @@ replace them with the notice and other provisions required by the GPL.
 If you do not delete the provisions above, a recipient may use your version
 of this file under either the MPL or the GPL.
 
-$Id: SynHighlighterDWS.pas,v 1.12 2011/09/27 06:23:05 Egg Exp $
-
-You may retrieve the latest version of this file at the SynEdit home page,
-located at http://SynEdit.SourceForge.net
 
 Known Issues:
 -------------------------------------------------------------------------------}
