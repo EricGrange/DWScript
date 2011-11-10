2d9aab506adce1635005e8ea7a51e1fad62082b6
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2011-11-10 14:55:14 +0000

    Added complex divide + test
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@1006 d49b5f25-dba9-2846-803f-502412c64022

--- Source/dwsMathComplexFunctions.pas
+++ Source/dwsMathComplexFunctions.pas
@@ -34,19 +34,29 @@ type
          procedure DoEvalAsString(args : TExprBaseList; var Result : UnicodeString); override;
    end;
 
-   TComplexBinOpExpr = class(TInternalMagicDataFunction);
+   TComplexOpExpr = class(TInternalMagicDataFunction);
 
-   TComplexAddOpExpr = class(TComplexBinOpExpr)
+   TComplexNegOpExpr = class(TComplexOpExpr)
       public
          procedure DoEval(args : TExprBaseList; var result : TDataPtr); override;
    end;
 
-   TComplexSubOpExpr = class(TComplexBinOpExpr)
+   TComplexAddOpExpr = class(TComplexOpExpr)
       public
          procedure DoEval(args : TExprBaseList; var result : TDataPtr); override;
    end;
 
-   TComplexMultOpExpr = class(TComplexBinOpExpr)
+   TComplexSubOpExpr = class(TComplexOpExpr)
+      public
+         procedure DoEval(args : TExprBaseList; var result : TDataPtr); override;
+   end;
+
+   TComplexMultOpExpr = class(TComplexOpExpr)
+      public
+         procedure DoEval(args : TExprBaseList; var result : TDataPtr); override;
+   end;
+
+   TComplexDivOpExpr = class(TComplexOpExpr)
       public
          procedure DoEval(args : TExprBaseList; var result : TDataPtr); override;
    end;
@@ -103,6 +113,7 @@ begin
    operators.RegisterOperator(ttPLUS, unitTable.FindSymbol('ComplexAdd', cvMagic) as TFuncSymbol, typComplex, typComplex);
    operators.RegisterOperator(ttMINUS, unitTable.FindSymbol('ComplexSub', cvMagic) as TFuncSymbol, typComplex, typComplex);
    operators.RegisterOperator(ttTIMES, unitTable.FindSymbol('ComplexMult', cvMagic) as TFuncSymbol, typComplex, typComplex);
+   operators.RegisterOperator(ttDIVIDE, unitTable.FindSymbol('ComplexDiv', cvMagic) as TFuncSymbol, typComplex, typComplex);
 end;
 
 // ------------------
@@ -136,6 +147,22 @@ begin
    else if i<0 then
       Result:=Format('%f - %fi', [r, Abs(i)])
    else Result:=Format('%f', [r]);
+end;
+
+// ------------------
+// ------------------ TComplexNegOpExpr ------------------
+// ------------------
+
+// DoEval
+//
+procedure TComplexNegOpExpr.DoEval(args : TExprBaseList; var result : TDataPtr);
+var
+   v : TDataPtr;
+begin
+   v:=TDataExpr(args.ExprBase[0]).DataPtr[args.Exec];
+
+   result[0]:=-v[0];
+   result[1]:=-v[1];
 end;
 
 // ------------------
@@ -189,6 +216,25 @@ begin
    result[1]:=leftData[1]*rightData[0]+leftData[0]*rightData[1];
 end;
 
+// ------------------
+// ------------------ TComplexDivOpExpr ------------------
+// ------------------
+
+// DoEval
+//
+procedure TComplexDivOpExpr.DoEval(args : TExprBaseList; var result : TDataPtr);
+var
+   leftData, rightData : TDataPtr;
+   d : Double;
+begin
+   leftData:=TDataExpr(args.ExprBase[0]).DataPtr[args.Exec];
+   rightData:=TDataExpr(args.ExprBase[1]).DataPtr[args.Exec];
+
+   d:=Sqr(rightData[0])+Sqr(rightData[1]);
+   result[0]:=(leftData[0]*rightData[0]+leftData[1]*rightData[1])/d;
+   result[1]:=(leftData[1]*rightData[0]-leftData[0]*rightData[1])/d;
+end;
+
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
@@ -203,8 +249,10 @@ initialization
    RegisterInternalFunction(TComplexMakeExpr, 'Complex', ['real', SYS_FLOAT, 'imaginary', SYS_FLOAT], SYS_COMPLEX, True);
    RegisterInternalStringFunction(TComplexToStrExpr, 'ComplexToStr', ['c', SYS_COMPLEX], True);
 
-   RegisterInternalFunction(TComplexAddOpExpr, 'ComplexAdd', ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
-   RegisterInternalFunction(TComplexSubOpExpr, 'ComplexSub', ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
+   RegisterInternalFunction(TComplexNegOpExpr,  'ComplexNeg',  ['v', SYS_COMPLEX], SYS_COMPLEX, True);
+   RegisterInternalFunction(TComplexAddOpExpr,  'ComplexAdd',  ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
+   RegisterInternalFunction(TComplexSubOpExpr,  'ComplexSub',  ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
    RegisterInternalFunction(TComplexMultOpExpr, 'ComplexMult', ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
+   RegisterInternalFunction(TComplexDivOpExpr,  'ComplexDiv',  ['left', SYS_COMPLEX, 'right', SYS_COMPLEX], SYS_COMPLEX, True);
 
 end.
---
+++ Test/FunctionsMathComplex/div.pas
@@ -0,0 +1,7 @@
+PrintLn(ComplexToStr( Complex(3, 0) / Complex(2, 0)));
+
+const i = Complex(0, 1);
+
+PrintLn(ComplexToStr( i / i ));
+
+PrintLn(ComplexToStr( Complex(2, 3) / Complex(4, 8)));
