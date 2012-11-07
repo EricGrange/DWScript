3e6f3facbe430f0f449bfb5c64597210a07e18d1
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2012-11-07 14:32:17 +0000

    New static symbols tests
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@1614 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/UdwsUnitTestsStatic.pas
@@ -0,0 +1,72 @@
+unit UdwsUnitTestsStatic;
+
+interface
+
+uses Classes, SysUtils, dwsXPlatformTests, dwsComp, dwsCompiler, dwsExprs,
+   dwsTokenizer, dwsSymbols, dwsUtils, dwsStack, UdwsUnitTests;
+
+type
+
+   TdwsUnitTestsStatic = class (TdwsUnitTests)
+      private
+         class var vContext : TdwsUnitTestsContext;
+
+         procedure DoAfterInitUnitTable(Sender : TObject);
+
+      public
+         procedure SetUp; override;
+         procedure TearDown; override;
+   end;
+
+implementation
+
+// ------------------
+// ------------------ TdwsUnitTestsStatic ------------------
+// ------------------
+
+// SetUp
+//
+procedure TdwsUnitTestsStatic.SetUp;
+begin
+   FContext:=vContext;
+
+   inherited SetUp;
+
+   vContext:=FContext;
+
+   FUnit.StaticSymbols:=True;
+
+   FUnit.OnAfterInitUnitTable:=DoAfterInitUnitTable;
+end;
+
+// TearDown
+//
+procedure TdwsUnitTestsStatic.TearDown;
+begin
+   FContext:=nil;
+
+   inherited TearDown;
+end;
+
+// DoAfterInitUnitTable
+//
+procedure TdwsUnitTestsStatic.DoAfterInitUnitTable(Sender : TObject);
+begin
+   FUnit.Tag:=FUnit.Tag+1;
+end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+initialization
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+   RegisterTest('dwsUnitTestsStatic', TdwsUnitTestsStatic);
+
+finalization
+
+   TdwsUnitTestsStatic.vContext.Free;
+
+end.
