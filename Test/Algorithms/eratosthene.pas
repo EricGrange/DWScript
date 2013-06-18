47f8614fd934a320cec2f5c90ed40bbfdd792c45
zarglu@gmail.com <zarglu@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2013-06-18 07:14:09 +0000

    Added sieve of eratosthene test (large set)
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@2180 d49b5f25-dba9-2846-803f-502412c64022

---
+++ Test/Algorithms/eratosthene.pas
@@ -0,0 +1,13 @@
+type TRange = enum (Low = 2, High = 1000);
+
+var sieve : set of TRange;
+
+var t := Now;
+var last : Integer;
+
+for var e in TRange do begin
+   if e in sieve then continue;
+   PrintLn(e.Value);
+   for var k:=e to TRange.High step e do
+      sieve.Include(k);
+end;
