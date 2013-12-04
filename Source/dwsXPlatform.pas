0b3d01c04716b64f7e52ecaee120f8f38f533c86
CWBudde@gmail.com <CWBudde@gmail.com@d49b5f25-dba9-2846-803f-502412c64022>
2013-12-04 13:37:13 +0000

    Some XPlatform changes
    git-svn-id: https://dwscript.googlecode.com/svn/trunk@2350 d49b5f25-dba9-2846-803f-502412c64022

--- Source/dwsXPlatform.pas
+++ Source/dwsXPlatform.pas
@@ -35,9 +35,18 @@ unit dwsXPlatform;
 
 interface
 
-uses Windows, Classes, SysUtils, Masks
-   {$IFNDEF VER200}, IOUtils{$ENDIF}
-   ;
+uses
+  Classes, SysUtils, Types, Masks,
+  {$IFDEF FPC}
+  {$IFDEF Windows}
+  Windows
+  {$ELSE}
+  LCLIntf
+  {$ENDIF}
+  {$ELSE}
+  Windows {$IFNDEF VER200}, IOUtils{$ENDIF}
+  {$ENDIF}
+  ;
 
 const
 {$IFDEF UNIX}
@@ -217,14 +226,23 @@ implementation
 uses Variants;
 {$endif}
 
+{$ifdef FPC}
+type
+   TFindExInfoLevels = FINDEX_INFO_LEVELS;
+{$endif}
+
 // GetSystemMilliseconds
 //
 function GetSystemMilliseconds : Int64;
 var
    fileTime : TFileTime;
 begin
+{$IFDEF Windows}
    GetSystemTimeAsFileTime(fileTime);
    Result:=Round(PInt64(@fileTime)^*1e-4); // 181
+{$ELSE}
+   Not yet implemented!
+{$ENDIF}
 end;
 
 // UTCDateTime
@@ -233,11 +251,15 @@ function UTCDateTime : TDateTime;
 var
    systemTime : TSystemTime;
 begin
+{$IFDEF Windows}
    FillChar(systemTime, SizeOf(systemTime), 0);
    GetSystemTime(systemTime);
    with systemTime do
       Result:= EncodeDate(wYear, wMonth, wDay)
               +EncodeTime(wHour, wMinute, wSecond, wMilliseconds);
+{$ELSE}
+   Not yet implemented!
+{$ENDIF}
 end;
 
 {$ifndef FPC}
@@ -401,7 +423,11 @@ end;
 function InterlockedExchangePointer(var target : Pointer; val : Pointer) : Pointer;
 {$ifndef WIN32_ASM}
 begin
+   {$ifdef FPC}
+   Result:=InterlockedExchangePointer(target, val);
+   {$else}
    Result:=Windows.InterlockedExchangePointer(target, val);
+   {$endif}
 {$else}
 asm
    lock  xchg dword ptr [eax], edx
@@ -724,7 +750,7 @@ end;
 
 // FileCopy
 //
-function FileCopy(const existing, new : String; failIfExists : Boolean) : Boolean;
+function FileCopy(const existing, new : UnicodeString; failIfExists : Boolean) : Boolean;
 begin
    Result:=Windows.CopyFileW(PWideChar(existing), PWideChar(new), failIfExists);
 end;
@@ -733,7 +759,7 @@ end;
 //
 function FileDelete(const fileName : String) : Boolean;
 begin
-   Result:=DeleteFile(fileName);
+   Result:=SysUtils.DeleteFile(fileName);
 end;
 
 // FileRename
