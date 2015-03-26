b8151830237929125855a65d443e8f5bcc95d56a
Eric Grange <egrange@creative-it.net>
2015-03-26 16:39:20 +0100

    Improved test coverage

--- Test/SimpleScripts/in_operator.pas
+++ Test/SimpleScripts/in_operator.pas
@@ -2,6 +2,15 @@ var i : Integer;
 for i:=1 to 10 do
    if i in [2, 4, 6..8] then
       PrintLn(i);
+for i:=1 to 10 do
+   if (i+1) in [1, 3, 5] then
+      PrintLn(i);
+for i:=1 to 10 do
+   if i in [7] then
+      PrintLn(i);
+for i:=1 to 10 do
+   if (i+1) in [10..12, 13] then
+      PrintLn(i);
 
 PrintLn(BoolToStr(1 in []));
 
@@ -10,3 +19,5 @@ if 'A' in ['A'..'Z'] then
 
 if 'a' in ['A'..'Z'] then
    PrintLn('a in A..Z');
+   
+
