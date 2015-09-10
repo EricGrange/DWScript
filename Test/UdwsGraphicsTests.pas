d56a4b7751edbb1ef7123fe9948756e1e7438384
Eric Grange <egrange@creative-it.net>
2015-09-10 13:48:06 +0200

    Fixed Issue #29

---
+++ Test/UdwsGraphicsTests.pas
@@ -0,0 +1,176 @@
+unit UdwsGraphicsTests;
+
+interface
+
+uses
+   Classes, SysUtils,
+   dwsXPlatformTests, dwsComp, dwsCompiler, dwsExprs, dwsErrors,
+   dwsGraphicLibrary, dwsXPlatform, dwsSymbols, dwsUtils,
+   dwsEncodingLibModule;
+
+type
+
+   TdwsGraphicsTests = class (TTestCase)
+      private
+         FTests : TStringList;
+         FCompiler : TDelphiWebScript;
+
+      public
+         procedure SetUp; override;
+         procedure TearDown; override;
+
+         procedure Execution;
+         procedure Compilation;
+
+      published
+
+         procedure CompilationNormal;
+         procedure CompilationWithMapAndSymbols;
+         procedure ExecutionNonOptimized;
+         procedure ExecutionOptimized;
+   end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+implementation
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+// ------------------
+// ------------------ TdwsGraphicsTests ------------------
+// ------------------
+
+// SetUp
+//
+procedure TdwsGraphicsTests.SetUp;
+begin
+   FTests:=TStringList.Create;
+
+   CollectFiles(ExtractFilePath(ParamStr(0))+'GraphicsLib'+PathDelim, '*.pas', FTests);
+
+   FCompiler:=TDelphiWebScript.Create(nil);
+
+   TdwsEncodingLib.Create(FCompiler).dwsEncoding.Script:=FCompiler;
+end;
+
+// TearDown
+//
+procedure TdwsGraphicsTests.TearDown;
+begin
+   FCompiler.Free;
+
+   FTests.Free;
+end;
+
+// Compilation
+//
+procedure TdwsGraphicsTests.Compilation;
+var
+   source : TStringList;
+   i : Integer;
+   prog : IdwsProgram;
+begin
+   source:=TStringList.Create;
+   try
+
+      for i:=0 to FTests.Count-1 do begin
+
+         source.LoadFromFile(FTests[i]);
+
+         prog:=FCompiler.Compile(source.Text);
+         CheckEquals('', prog.Msgs.AsInfo, FTests[i]);
+
+      end;
+
+   finally
+      source.Free;
+   end;
+end;
+
+// CompilationNormal
+//
+procedure TdwsGraphicsTests.CompilationNormal;
+begin
+   FCompiler.Config.CompilerOptions:=[coOptimize];
+   Compilation;
+end;
+
+// CompilationWithMapAndSymbols
+//
+procedure TdwsGraphicsTests.CompilationWithMapAndSymbols;
+begin
+   FCompiler.Config.CompilerOptions:=[coSymbolDictionary, coContextMap, coAssertions];
+   Compilation;
+end;
+
+// ExecutionNonOptimized
+//
+procedure TdwsGraphicsTests.ExecutionNonOptimized;
+begin
+   FCompiler.Config.CompilerOptions:=[coAssertions];
+   Execution;
+end;
+
+// ExecutionOptimized
+//
+procedure TdwsGraphicsTests.ExecutionOptimized;
+begin
+   FCompiler.Config.CompilerOptions:=[coOptimize, coAssertions];
+   Execution;
+end;
+
+// Execution
+//
+procedure TdwsGraphicsTests.Execution;
+var
+   source, expectedResult : TStringList;
+   i : Integer;
+   prog : IdwsProgram;
+   exec : IdwsProgramExecution;
+   resultText, resultsFileName : String;
+begin
+   source:=TStringList.Create;
+   expectedResult:=TStringList.Create;
+   try
+
+      for i:=0 to FTests.Count-1 do begin
+
+         source.LoadFromFile(FTests[i]);
+
+         prog:=FCompiler.Compile(source.Text);
+
+         CheckEquals('', prog.Msgs.AsInfo, FTests[i]);
+         exec:=prog.Execute;
+
+         resultText:=exec.Result.ToString;
+         if exec.Msgs.Count>0 then
+            resultText:=resultText+#13#10'>>>> Error(s): '#13#10+exec.Msgs.AsInfo;
+
+         resultsFileName:=ChangeFileExt(FTests[i], '.txt');
+         if FileExists(resultsFileName) then begin
+            expectedResult.LoadFromFile(resultsFileName);
+            CheckEquals(expectedResult.Text, resultText, FTests[i]);
+         end else CheckEquals('', resultText, FTests[i]);
+         CheckEquals('', exec.Msgs.AsInfo, FTests[i]);
+
+      end;
+
+   finally
+      expectedResult.Free;
+      source.Free;
+   end;
+end;
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+initialization
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+
+   RegisterTest('LibModules', TdwsGraphicsTests);
+
+end.
