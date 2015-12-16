bed1fd45ca8db09d66a4c669f8ee4b009d8f7911
Eric Grange <egrange@creative-it.net>
2015-12-16 08:59:25 +0100

    Fixed test case result

--- Test/SimpleScripts/const_array_nested1.pas
+++ Test/SimpleScripts/const_array_nested1.pas
@@ -1,6 +1,6 @@
 const cColors = [ ['#df0049', '#660671'], ['#ffd200', '#b06c00'] ];
 
-Print(cColors[0][0]);
-Print(cColors[0][1]);
-Print(cColors[1][0]);
-Print(cColors[1][1]);
+PrintLn(cColors[0][0]);
+PrintLn(cColors[0][1]);
+PrintLn(cColors[1][1]);
+PrintLn(cColors[1][0]);
