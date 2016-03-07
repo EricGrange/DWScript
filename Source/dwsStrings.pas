737efa2f03b81ee40a3a694d6aa04fd70bab7661
Eric Grange <egrange@creative-it.net>
2016-03-07 13:45:36 +0100

    Spellfix

--- Source/dwsStrings.pas
+++ Source/dwsStrings.pas
@@ -496,7 +496,7 @@ const
   TOK_GreaterEqualityExpected = '">" or "=" expected';
   TOK_StringTerminationError = 'End of string constant not found (end of line)';
   TOK_HereDocTerminationError = 'End of string constant not found (end of file)';
-  TOK_InvalidHexConstant = 'Invalid hexadezimal constant "%s"';
+  TOK_InvalidHexConstant = 'Invalid hexadecimal constant "%s"';
   TOK_InvalidCharConstant = 'Invalid char constant "%s"';
   TOK_InvalidIntegerConstant = 'Invalid integer constant "%s"';
   TOK_InvalidFloatConstant = 'Invalid floating point constant "%s"';
