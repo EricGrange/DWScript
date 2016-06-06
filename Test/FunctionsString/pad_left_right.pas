5193d23d5e8d78f4c6b1a5188a7f505214ab0e36
Eric Grange <egrange@creative-it.net>
2016-06-06 08:59:42 +0200

    Extra test case

--- Test/FunctionsString/pad_left_right.pas
+++ Test/FunctionsString/pad_left_right.pas
@@ -2,3 +2,7 @@ for var i := 1 to 3 do
 	PrintLn(PadLeft('aa', i)+','+PadRight('bb', i)+';');
 for var i := 1 to 3 do 
 	PrintLn(PadLeft('aa', i, 'z')+','+PadRight('bb', i, 'z')+';');
+
+var s := '1234567';
+PrintLn(s.PadLeft(8, '0'));
+PrintLn(s.PadRight(8, '8'));
