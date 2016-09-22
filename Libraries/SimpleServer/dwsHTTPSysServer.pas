71a1a9e280d981d6e572e2e7022e1e4fe1a058b7
Eric Grange <egrange@creative-it.net>
2016-09-22 14:07:43 +0200

    Removed unused Before/AfterWaitForNextRequest

--- Libraries/SimpleServer/dwsHTTPSysServer.pas
+++ Libraries/SimpleServer/dwsHTTPSysServer.pas
@@ -233,8 +233,6 @@ type
                                         var inContent : RawByteString) : Boolean;
 
          function GetHttpResponseFlags: Cardinal; virtual;
-         procedure BeforeWaitForNextRequest; virtual;
-         procedure AfterWaitForNextRequest(const aCurRequest: PHTTP_REQUEST_V2); virtual;
 
          function GetLogging : Boolean; inline;
          procedure SetLogging(const val : Boolean);
@@ -795,16 +793,6 @@ begin
    end;
 end;
 
-procedure THttpApi2Server.AfterWaitForNextRequest(const aCurRequest: PHTTP_REQUEST_V2);
-begin
-//
-end;
-
-procedure THttpApi2Server.BeforeWaitForNextRequest;
-begin
-//
-end;
-
 procedure THttpApi2Server.Clone(ChildThreadCount : Integer);
 var
    i : Integer;
@@ -1021,12 +1009,10 @@ begin
    // main loop
    requestID := 0;
    repeat
-      BeforeWaitForNextRequest;
       // retrieve next pending request, and read its headers
       FillChar(request^, SizeOf(HTTP_REQUEST_V2), 0);
       errCode := HttpAPI.ReceiveHttpRequest(FReqQueue, requestID, 0, request^,
                                             Length(requestBuffer), bytesRead);
-      AfterWaitForNextRequest(request);
       if Terminated then
          break;
       case errCode of
