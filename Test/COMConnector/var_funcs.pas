eb7c30f3e9227f1c9714af652f0634dd6503bd5e
Eric Grange <egrange@creative-it.net>
2017-01-13 14:18:44 +0100

    Improved test coverage

--- Test/COMConnector/var_funcs.pas
+++ Test/COMConnector/var_funcs.pas
@@ -1,5 +1,18 @@
-var a : ComVariant;
+﻿var a : ComVariant;
+
+a := 1;
+PrintLn(VarIsClear(a));
+PrintLn(VarIsArray(a));
+PrintLn(VarType(a));
 
 VarClear(a);
-
+PrintLn(VarIsClear(a));
+PrintLn(VarIsArray(a));
 PrintLn(VarType(a));
+
+var v : ComVariantArray;
+a := v;
+PrintLn(VarIsClear(a));
+PrintLn(VarIsArray(a));
+PrintLn(VarType(a));
+
