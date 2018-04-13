711700a733db2b7928ee6944324e18e9e0a1db99
egrange <zarglu@gmail.com>
2018-04-13 11:29:24 +0200

    Made test less sensitive to exact format message as it changed across Delphi versions

--- Test/FunctionsString/format_typecheck.pas
+++ Test/FunctionsString/format_typecheck.pas
@@ -5,5 +5,5 @@ try
    temp := Format('%d %d', [ID, ID]);
 except
    on E: Exception do 
-      PrintLn(E.ClassName+': '+E.Message);
+      PrintLn(E.ClassName+': '+E.Message.Before("'")+E.Message.After("' "));
 end;
