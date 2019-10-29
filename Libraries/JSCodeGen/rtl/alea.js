53f3d2e19719924befe0cd11cbe31cba6e327026
egrange <zarglu@gmail.com>
2019-10-29 14:45:34 +0100

    Cleanup leftover comments

--- Libraries/JSCodeGen/rtl/alea.js
+++ Libraries/JSCodeGen/rtl/alea.js
@@ -1,6 +1,6 @@
 /*
 
-Copyright (C) 2010 by Johannes Baagøe <baagoe@baagoe.org>
+Copyright (C) 2010 by Johannes Baagoe <baagoe@baagoe.org>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,18 +24,13 @@ From http://baagoe.com/en/RandomMusings/javascript/
 */
 function $alea() {
   return (function(args) {
-    // Johannes BaagÃ¸e <baagoe@baagoe.com>, 2010
-    var s0 = 0;
-    var s1 = 0;
-    var s2 = 0;
-    var c = 1;
+    var s0 = 0, s1 = 0, s2 = 0, c = 1;
 
     if (args.length == 0) {
       args = [+new Date];
     }
     var mash = function() {
        var n = 0xefc8249d;
-    
        var mash = function(data) {
          data = data.toString();
          for (var i = 0; i < data.length; i++) {
@@ -50,8 +45,6 @@ function $alea() {
          }
          return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
        };
-    
-       //mash.version = 'Mash 0.9';
        return mash;
     }();
     s0 = mash(' ');
@@ -80,14 +73,6 @@ function $alea() {
       s1 = s2;
       return s2 = t - (c = t | 0);
     };
-    /*random.uint32 = function() {
-      return random() * 0x100000000; // 2^32
-    };
-    random.fract53 = function() {
-      return random() +
-        (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
-    };*/
-    //random.version = 'Alea 0.9';
     random.args = args;
     return random;
 
