fe9e2205fd5524c0409b3811b699991435abd0e5
egrange <zarglu@gmail.com>
2020-05-25 10:34:54 +0200

    Improved cmpxchg test

--- Test/FunctionsGlobalVars/cmpxchg.pas
+++ Test/FunctionsGlobalVars/cmpxchg.pas
@@ -4,6 +4,11 @@ PrintLn(ReadGlobalVar('test'));
 PrintLn(CompareExchangeGlobalVar('test', 2, 1));
 PrintLn(ReadGlobalVar('test'));
 
+WriteGlobalVar('test', 0);
+DeleteGlobalVar('test');
+PrintLn(CompareExchangeGlobalVar('test', 0, Unassigned)=Unassigned);
+PrintLn(ReadGlobalVar('test'));
+
 WriteGlobalVar('test', 1);
 
 PrintLn(ReadGlobalVar('test'));
