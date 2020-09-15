887891b3be98d4839198d342960156e0a550279d
egrange <zarglu@gmail.com>
2020-09-15 12:58:55 +0200

    Added SQLite base64() test cases

---
+++ Test/DataBaseLib/sqlite_base64.pas
@@ -0,0 +1,8 @@
+﻿var db := new DataBase('SQLite');
+
+PrintLn(db.Query("select base64(null)").IsNull(0));
+PrintLn(db.Query("select base64(1)").AsString(0));
+PrintLn(db.Query("select base64(1.0)").AsString(0));
+PrintLn(db.Query("select base64('hello')").AsString(0));
+PrintLn(db.Query("select base64(x'01')").AsString(0));
+PrintLn(db.Query("select base64(x'010203040506')").AsString(0));
