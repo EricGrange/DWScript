b5485e28b0b1a4b89376b27b3f8b6e7da024980b
egrange <zarglu@gmail.com>
2020-12-02 11:22:24 +0100

    Reduced BigInteger pool contention when the pool is empty

--- Libraries/BigIntegersLib/dwsBigIntegerFunctions.GMP.pas
+++ Libraries/BigIntegersLib/dwsBigIntegerFunctions.GMP.pas
@@ -712,14 +712,11 @@ end;
 //
 function TdwsBigIntegerWrapperPool.Pop : TBigIntegerWrapper;
 begin
-   if Self = nil then
-      Result := TBigIntegerWrapper.CreateNewZero
-   else begin
+   Result := nil;
+   if Self <> nil then begin
       FLock.BeginWrite;
       try
-         if FHead = nil then
-            Result := TBigIntegerWrapper.CreateNewZero
-         else begin
+         if FHead <> nil then begin
             Result := FHead;
             FHead := FHead.FNext;
             Result.FNext := nil;
@@ -729,6 +726,8 @@ begin
          FLock.EndWrite;
       end;
    end;
+   if Result = nil then
+      Result := TBigIntegerWrapper.CreateNewZero
 end;
 
 // Push
