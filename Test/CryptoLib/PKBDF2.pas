151831f9a1f4f1239584a072278089c56b2dde64
egrange <zarglu@gmail.com>
2021-06-21 16:43:01 +0200

    Updated test vectors & expected results

--- Test/CryptoLib/PKBDF2.pas
+++ Test/CryptoLib/PKBDF2.pas
@@ -1,5 +1,5 @@
 
-PrintLn(PBKDF2_HMAC_SHA256('password', 'hello', 1000000));
+PrintLn(PBKDF2_HMAC_SHA256('password', 'salt', 4096).Left(40));
 
 PrintLn(PBKDF2_HMAC_SHA256('this is some password longer than 32 bytes', 'this is some salt longer than 32 bytes', 20));
 
