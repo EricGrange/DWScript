33e8eaa4fb53596b07770aee68a19f1ad47c97d7
egrange <zarglu@gmail.com>
2022-03-15 09:23:15 +0100

    Added test case for integer array coverage of shrink method

---
+++ Test/ArrayPass/shrink_integer_array.pas
@@ -0,0 +1,14 @@
+﻿var a : array of Integer;
+
+for var i := 1 to 100 do
+   a.Add(i);
+
+a.Delete(10, 80);
+PrintLn(a.Map(IntToStr).Join(','));
+
+a.SetLength(100);
+PrintLn(a.Length);
+
+a.SetLength(10);
+PrintLn(a.Map(IntToStr).Join(','));
+
