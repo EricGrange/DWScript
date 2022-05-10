b7195df4de385b03e959fc05c581ef7a7ad3b706
egrange <zarglu@gmail.com>
2022-05-10 11:24:50 +0200

    Improved tests coverage for character in static selection

---
+++ Test/SimpleScripts/alphanum.pas
@@ -0,0 +1,22 @@
+﻿
+procedure NumbersAndVowels(s : String);
+begin
+   var nums, vows, accented : Integer;
+   for var i := 1 to Length(s) do begin
+      if s[i] in ['a', 'e', 'i', 'o', 'u', 'y'] then
+         vows += 1;
+      if s[i] in ['0'..'9'] then
+         nums += 1;
+      if s[i] in ['à', 'é'] then
+         accented += 1;
+   end;
+   PrintLn(nums.ToString + ', ' + vows.ToString + ', ' + accented.ToString);
+end;
+
+NumbersAndVowels('');
+NumbersAndVowels('hello world');
+NumbersAndVowels('42 is the answer');
+NumbersAndVowels('éléphant 3');
+
+
+
