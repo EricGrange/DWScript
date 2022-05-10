65ab46ac8f2ef2dfd2d467d636733d20b5ceef0f
egrange <zarglu@gmail.com>
2022-05-10 10:28:35 +0200

    Improve test coverage for dwsArrayElementContext

--- Test/ArrayPass/array_of_rec_fields.pas
+++ Test/ArrayPass/array_of_rec_fields.pas
@@ -7,6 +7,7 @@ type
 	B2: Boolean;
 	B3: Boolean;
 	V: Variant;
+	O1, O2: TObject;
   end;
 
 var tmpA: array of TRec;
@@ -19,6 +20,8 @@ tmpA[0].F := 1.5;
 tmpA[0].B2 := False;
 tmpA[0].B3 := True;
 tmpA[0].V := Null;
+tmpA[0].O1 := new TObject;
+tmpA[0].O2 := nil;
 PrintLn(if tmpA[0].S = 'hello' then 'Hello');
 PrintLn(tmpA[0].B1 = True);
 PrintLn(if tmpA[0].I = 123 then 1234);
@@ -26,3 +29,5 @@ PrintLn(if tmpA[0].F = 1.5 then 1.25);
 PrintLn(tmpA[0].B2 = True);
 PrintLn(tmpA[0].B3 = True);
 PrintLn(if VarIsNull(tmpA[0].V) then 'null');
+PrintLn(tmpA[0].O1.ClassName);
+PrintLn(tmpA[0].O2 = nil);
