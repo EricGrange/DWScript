a4a460e8c01a5b201dd5199bf69f634aa112c81b
egrange <zarglu@gmail.com>
2022-08-23 10:16:01 +0200

    More thorough tests for boolean xor & Odd()

--- Test/FunctionsMath/OddEven.pas
+++ Test/FunctionsMath/OddEven.pas
@@ -1,2 +1,9 @@
 if Odd(2) then PrintLn('2 not odd');
 if not Odd(1) then PrintLn('1 is odd');
+
+for var i := -5 to 5 do begin
+	if Odd(i) <> ((i and 1) <> 0) then PrintLn(i.ToString + ' bug');
+	if Odd(i) then
+		if (i and 1) = 0 then 
+			PrintLn(i.ToString + ' rebug');
+end;
---
+++ Test/SimpleScripts/bool_xor.pas
@@ -0,0 +1,18 @@
+﻿var a, b, c : Boolean;
+b := True;
+
+for var i := 0 to 1 do begin
+   c := a xor (i = 0);
+	Print(Ord(c));
+   c := b xor (i = 0);
+	Print(Ord(c));
+   c := (i = 0) xor a;
+	Print(Ord(c));
+   c := (i = 0) xor b;
+	PrintLn(Ord(c));
+	
+   if a xor (i = 0) then Print(2) else Print(0);
+   if b xor (i = 0) then Print(2) else Print(0);
+   if (i = 0) xor a then Print(2) else Print(0);
+	if (i = 0) xor b then PrintLn(2) else PrintLn(0);
+end;
